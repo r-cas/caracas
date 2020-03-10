b3bbb588436fd5ace868803d363a15d159e4b2e0
Mikkel Meyer Andersen <mikl@mikl.dk>
2020-03-10 22:51:59 +0100

    Work on #10

---
+++ R/subset.R
@@ -0,0 +1,66 @@
+convert_to_r_mat <- function(x) {
+  xmat <- matrix(NA_character_, nrow = nrow(x), ncol = ncol(x))
+  xlst <- x$pyobj$tolist()
+  
+  for (i in seq_along(xlst)) {
+    row_i <- xlst[[i-1L]] # 0-based
+    
+    for (j in seq_along(row_i)) {
+      xmat[i, j] <- as.character(row_i[[j - 1L]]) # 0-based
+    }
+  }
+  
+  return(xmat)
+}
+
+# convert_to_r_vec <- function(x) {
+#   xmat <- vector(NA_character_, length = length(x))
+#   xlst <- x$pyobj$tolist()
+#   
+#   for (i in seq_along(xlst)) {
+#     row_i <- xlst[[i-1L]] # 0-based
+#     
+#     for (j in seq_along(row_i)) {
+#       xmat[i, j] <- as.character(row_i[[j - 1L]]) # 0-based
+#     }
+#   }
+#   
+#   return(xmat)
+# }
+
+#' Extract or replace parts of an object
+#' 
+#' @param x A `yac_symbol`.
+#' @param i row indices specifying elements to extract or replace
+#' @param j column indices specifying elements to extract or replace
+#' 
+#' @examples 
+#' if (have_sympy()) {
+#'   A <- matrix(c("a", 0, 0, 0, "a", "a", "a", 0, 0), 3, 3)
+#'   B <- as_symbol(A)
+#'   B[1:2, ]
+#'   B[, 2]
+#'   B[2, , drop = FALSE]
+#' }
+#' 
+#' @export
+`[.caracas_symbol` <- function(x, i, j, ..., drop = TRUE) {
+  ensure_sympy()
+  
+  if (!inherits(x, "caracas_symbol")) {
+    stop(paste0("'x' ", TXT_NOT_CARACAS_SYMBOL))
+  }
+  
+  if (!symbol_is_matrix(x) && !symbol_is_vector(x)) {
+    stop("'x' must be a vector or matrix")
+  }
+  
+  if (symbol_is_matrix(x)) {
+    xmat <- convert_to_r_mat(x)
+    xmat_subset <- base::`[`(xmat, i, j, ..., drop = drop)
+    y <- as_symbol(xmat_subset)
+    return(y)
+  }
+  
+  stop("Not yet implemented")
+}
