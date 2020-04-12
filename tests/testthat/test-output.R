65bd9b1325f84f1464386cc27e553613ca4bd037
Mikkel Meyer Andersen <mikl@mikl.dk>
2020-04-12 21:52:53 +0200

    Test (utf8)

--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -97,6 +97,6 @@ test_that("print vector", {
   skip_if_no_sympy()
   
   B <- as_symbol(1:3)
-  str <- paste0("[caracas]: [1  2  3]", intToUtf8(7488L)) # 7488 -> transpose utf-8
-  expect_equal(capture.output(print(B)), str)
+  expect_true(grepl("^\\[caracas\\]: \\[1  2  3\\].+$", 
+                    capture.output(print(B)))) # .{1} == transpose 
 })
