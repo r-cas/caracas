f131444967a3232c601fc3f83e2aa3b7b1c190c1
Søren Højsgaard <sorenh@math.aau.dk>
2022-09-29 13:59:12 +0200

    SH: SH additions to simplify

--- R/simplify.R
+++ R/simplify.R
@@ -1,3 +1,9 @@
+## FIXME:
+## Concept is simplifcation
+## Need seealso's..
+## Can some entries be combined?
+## See https://docs.sympy.org/latest/tutorials/intro-tutorial/simplification.html
+
 #' Simplify expression
 #'
 #' @param x A `caracas_symbol`
@@ -18,6 +24,74 @@ simplify <- function(x) {
   return(v)
 }
 
+#' Collects common powers of a term in an expression
+#'
+#' @param x,a A `caracas_symbol`
+#' @examples 
+#' if (has_sympy()){
+#'   def_sym(x, y, z)
+#'   expr = x*y + x - 3 + 2*x**2 - z*x**2 + x**3
+#'   collect(expr, x)
+#' }
+
+#' @concept simplify
+#'
+#' @export
+collect <- function(x, a) {
+  stopifnot_symbol(x)
+  ensure_sympy()
+  sympy_func(x, "collect", a)
+}
+
+#' Put rational function into standard form
+#' 
+#' cancel() will take any rational function and put it into the standard canonical form
+#'
+#' @param x A `caracas_symbol`
+#' @examples 
+#' if (has_sympy()){
+#'   def_sym(x, y, z)
+#'   expr = cancel((x**2 + 2*x + 1)/(x**2 + x))
+#'   cancel(expr)
+#'   expr = (x*y**2 - 2*x*y*z + x*z**2 + y**2 - 2*y*z + z**2)/(x**2 - 1)
+#'   cancel(expr)
+#'   factor(expr)
+#' }
+
+#' @concept simplify
+#'
+#' @export
+cancel <- function(x) {
+  stopifnot_symbol(x)
+  ensure_sympy()
+  sympy_func(x, "cancel")
+}
+
+
+#' Partial fraction decomposition on a rational function
+#' 
+#' apart() performs a partial fraction decomposition on a rational function
+#'
+#' @param x A `caracas_symbol`
+#' @examples 
+#' if (has_sympy()){
+#'   def_sym(x)
+#'   expr = (4*x**3 + 21*x**2 + 10*x + 12)/(x**4 + 5*x**3 + 5*x**2 + 4*x)
+#'   apart(expr)
+#' }
+
+#' @concept simplify
+#'
+#' @export
+apart <- function(x) {
+  stopifnot_symbol(x)
+  ensure_sympy()
+  sympy_func(x, "apart")
+}
+
+
+
+
 
 
 #' Expand expression
@@ -28,17 +102,38 @@ simplify <- function(x) {
 #'
 #' @export
 expand <- function(x) {
-  if (!inherits(x, "caracas_symbol")) {
-    stop(paste0("'x' ", TXT_NOT_CARACAS_SYMBOL))
-  }
-  
+  stopifnot_symbol(x)
   ensure_sympy()
   
   z <- get_sympy()$expand(x$pyobj)
   v <- construct_symbol_from_pyobj(z)
-  
   return(v)
 }
+
+## FIXME: There is also a factor_list() function
+#' Expand expression
+#' 
+#' @param x A `caracas_symbol`
+#'
+#' @concept simplify
+#' @examples 
+#' 
+#' if (has_sympy()){
+#'   def_sym(x, y, z)
+#'   factor(x**3 - x**2 + x - 1)
+#'   factor(x**2*z + 4*x*y*z + 4*y**2*z)
+#' }
+#'
+#' @export
+factor <- function(x) {
+  ensure_sympy()
+  stopifnot_symbol(x)
+  sympy_func(x, "factor")
+}
+
+
+
+
 
 
 #' Expand a trigonometric expression
@@ -95,8 +190,6 @@ expand_log <- function(x) {
   
   return(v)
 }
-
-
 
 #' Expand a function expression
 #'
