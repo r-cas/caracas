ad9d5484cebb71dec6f3163b8dad8c65505febf8
Mikkel Meyer Andersen <mikl@mikl.dk>
2023-08-15 21:08:40 +0200

    Allowing multiple outputs in knitr rtex chunk engine

--- R/zzz.R
+++ R/zzz.R
@@ -20,22 +20,115 @@ NULL
   if (requireNamespace("knitr", quietly = TRUE)) {
     # Avoid global variables
     local({
-      R_engine  <- knitr::knit_engines$get("R")
-      
       prefix <- "cat(paste0('$$', tex("
       postfix <- "), '$$'))"
       
+      R_engine  <- knitr::knit_engines$get("R")
       knitr::knit_engines$set(rtex = function(options) {
-        options$code <- paste0(prefix, options$code, postfix)
-        options$results <- "asis"
-        y <- R_engine(options)
-        
-        if (options$echo) {
-          y <- gsub(prefix, "", y, fixed = TRUE)
-          y <- gsub(postfix, "", y, fixed = TRUE)
+        if (length(options$code) == 0L) {
+          return(R_engine(options))
         }
         
-        y
+        # First version is to assume that the chunk is one line and do:
+        # options$code <- paste0("cat(paste0('$$', tex(", options$code, "), '$$'))")
+        # options$results <- "asis"
+        # y <- R_engine(options)
+        # if (options$echo) {
+        #  y <- gsub("cat(paste0('$$', tex(", "", y, fixed = TRUE)
+        #  y <- gsub("), '$$'))", "", y, fixed = TRUE)
+        # }
+        # y
+        
+        # To support non-print lines and multiple outputs, 
+        # we need multiple steps:
+        # 1) Disable tidy
+        # 2) Run 'R_engine' and find matching lines
+        org_tidy <- options$tidy
+        org_echo <- options$echo
+        
+        options$tidy <- FALSE
+        options$echo <- TRUE
+        
+        input <- options$code
+        output <- R_engine(options)
+        o_l <- strsplit(output, "\n", fixed = TRUE)[[1L]]
+        
+        ###
+        # cat("===== input> =====\n")
+        # print(input)
+        # cat("===== <input =====\n")
+        # 
+        # cat("===== output> =====\n")
+        # print(o_l)
+        # cat("===== <output =====\n")
+        # 
+        
+        #print(o_l)
+        
+        # Detect output lines, i.e. lines in output that are not in input.
+        # They are used because the code preceding those lines
+        # need to be wrapped with tex() as they cause the output.
+        # FIXME: This can probably be optimised.
+        #        E.g.: Instead of searching all input, only allow each input
+        #              one time.
+        
+        # Which of o_l are output?
+        candidates <- seq_along(input)
+        lines_output <- logical(length(o_l))
+        
+        for (i in seq_along(o_l)) {
+          idx <- match(o_l[i], input[candidates], incomparables = c(""))
+          
+          # If line not found in input, then it's output:
+          if (length(idx) == 1L && is.na(idx)) {
+            lines_output[i] <- TRUE
+          } else {
+            candidates <- candidates[-idx]
+          }
+        }
+        #print(lines_output)
+        #cat("===== done =====")
+        
+        # Only code lines with output after needs mod
+        lines_needing_mod <- logical(length(o_l))
+        
+        for (i in seq_along(o_l)[-1L]) {
+          if (lines_output[i] && !lines_output[i-1L]) {
+            lines_needing_mod[i-1L] <- TRUE
+          }
+        }
+        
+        # for (i in seq_along(o_l)) {
+        #   p1 <- if (lines_output[i]) "OUTPUT" else "CODE  "
+        #   p2 <- if (lines_needing_mod[i]) "MOD " else "    "
+        #   p <- paste0(p1, p2)
+        #   cat(p, ": ", o_l[i], "\n", sep = "")
+        # }
+        
+        input_generating_tex <- match(o_l[lines_needing_mod], input)
+        #print(input_generating_tex)
+        new_input <- input
+        new_input[input_generating_tex] <- paste0(prefix, new_input[input_generating_tex], postfix)
+
+        new_options <- options
+        new_options$code <- new_input
+        new_options$results <- "asis"
+        new_options$engine <- "r" # for syntax highlighting
+        new_options$echo <- org_echo
+        new_output <- R_engine(new_options)
+        
+        if (options$echo) {
+          new_output <- gsub(prefix, "", new_output, fixed = TRUE)
+          new_output <- gsub(postfix, "", new_output, fixed = TRUE)
+        }
+        
+        #cat("\n\n\n\n")
+        #cat("====================\n")
+        #print(new_output)
+        #cat("====================\n")
+        #cat("\n\n\n\n")
+        
+        return(new_output)
       })
     })
   }
